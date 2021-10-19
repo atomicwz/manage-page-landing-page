85ff92cd9156c6b6c985c8a4d67851c47b72fdaf
Dener Oliveira <78823732+atomicwz@users.noreply.github.com>
2021-10-18 22:23:23 -0300

    Slide js

--- js/script.js
+++ js/script.js
@@ -10,3 +10,38 @@ const nav = document.getElementById('menu')
 }
 
 btn.addEventListener('click', menuHamburg)
+
+
+// Slide responsivo
+
+const box = document.querySelectorAll('#slide article')
+const slideBtn = document.querySelectorAll('.slide-button button')
+
+let i = 0
+let tempo = 3000
+
+function carrousel(){
+
+   slideBtn[i].classList.remove('red')
+   box[i].classList.remove("select")
+
+   i++
+
+   if( i >= box.length){
+      i = 0
+   }
+
+   box[i].classList.add("select")
+
+   slideBtn[i].classList.add('red')
+
+}
+
+
+
+function iniciarSlide(){ 
+   setInterval(carrousel, tempo)
+}
+
+window.addEventListener('load', iniciarSlide)
+
